fb39bd0a64c653c99fb2148ff17e8727cef3f802
ArslonBek_Ro'ziboyev <94825935+arslon1234@users.noreply.github.com>
2024-09-09 16:53:51 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div>
-   <h1 className="text-red-500 text-[20px] font-bold">Hello world</h1>
-     <p>learn github</p>
-      <h2>nimadir qo'shildi</h2>
-</div>
-    
+   <main>
+     <h1 className="text-red-500 text-[20px] font-bold">Hello world</h1>
+    </main>
   );
 }
