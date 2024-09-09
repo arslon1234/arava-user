44fb490f2c8ea9e4c0e5fa4fd882ba8c8062640e
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2024-09-09 15:05:52 +0500

    Checking

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Hello world</h1>
+    <main>
+     <h1 className="text-red-500 text-[20px] font-bold">Hello world</h1>
     </main>
   );
 }
