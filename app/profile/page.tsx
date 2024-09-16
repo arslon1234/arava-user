051ed8feeb7d55637066c4f6aeb5ed628772e0be
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2024-09-16 17:00:04 +0500

    Updated

---
+++ app/profile/page.tsx
@@ -0,0 +1,151 @@
+"use client";
+import React, { useState } from "react";
+import Container from "@/src/containers/container";
+
+const Cart = () => {
+  const [name, setName] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [errors, setErrors] = useState<{ name: string; phone_number: string }>({
+    name: "",
+    phone_number: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const validatePhone = (phone_number: string): string => {
+    return phone_number.length === 16
+      ? ""
+      : "+998 XX XXX XXXX formatida kiriting";
+  };
+
+  const validateName = (name: string): string => {
+    return name.length >= 3 ? "" : "Ismingizni kiriting (kamida 3 ta belgi)";
+  };
+
+  const validateForm = () => {
+    const nameError = validateName(name);
+    const phonenumberError = validatePhone(phone_number);
+
+    setErrors({ name: nameError, phone_number: phonenumberError });
+
+    return !nameError && !phonenumberError;
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = e.target.value;
+    setName(newName);
+
+    if (isSubmitted) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: validateName(newName),
+      }));
+    }
+  };
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let newPhoneNumber = e.target.value;
+
+    if (!newPhoneNumber.startsWith("+998")) {
+      newPhoneNumber =
+        "+998" + newPhoneNumber.replace(/[^\d]/g, "").substring(0, 9);
+    }
+
+    newPhoneNumber = newPhoneNumber.replace(/[^\d+]/g, "");
+
+    if (newPhoneNumber.length > 4) {
+      newPhoneNumber =
+        newPhoneNumber.slice(0, 4) + " " + newPhoneNumber.slice(4);
+    }
+    if (newPhoneNumber.length > 7) {
+      newPhoneNumber =
+        newPhoneNumber.slice(0, 7) + " " + newPhoneNumber.slice(7);
+    }
+    if (newPhoneNumber.length > 11) {
+      newPhoneNumber =
+        newPhoneNumber.slice(0, 11) + " " + newPhoneNumber.slice(11);
+    }
+
+    setPhoneNumber(newPhoneNumber);
+
+    if (isSubmitted) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        phone_number: validatePhone(newPhoneNumber),
+      }));
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+    if (validateForm()) {
+      const formattedPhoneNumber = phone_number.replace(/\s+/g, "");
+      const formData = {
+        name: name,
+        phone_number: formattedPhoneNumber,
+      }
+      console.log(formData);
+    }
+  };
+
+  return (
+    <section className="min-h-screen pt-[100px] lg:pt-[130px]">
+      <Container>
+        <form id="form" onSubmit={handleSubmit}>
+          <div className="mb-6 relative">
+            <input
+              type="text"
+              placeholder="Ismingiz"
+              className={`border-2 ${
+                errors.name
+                  ? "border-red-500 focus:border-red-500"
+                  : "border-gray-400"
+              } focus:border-mainColor rounded-lg md:rounded-xl py-2 px-3 w-full focus:outline-none`}
+              value={name}
+              onChange={handleNameChange}
+            />
+            {errors.name && (
+              <p className="absolute text-red-500 text-[13px]">{errors.name}</p>
+            )}
+          </div>
+
+          <div className="relative">
+            <input
+              className={`border-2 ${
+                errors.phone_number
+                  ? "border-red-500 focus:border-red-500"
+                  : "border-gray-400"
+              } focus:border-mainColor rounded-lg md:rounded-xl py-2 px-3 w-full focus:outline-none`}
+              type="text"
+              placeholder="Telefon raqamingiz"
+              value={phone_number}
+              onChange={handlePhoneChange}
+              maxLength={16}
+            />
+            {errors.phone_number && (
+              <p className="absolute text-red-500 text-[13px]">
+                {errors.phone_number}
+              </p>
+            )}
+          </div>
+        </form>
+      </Container>
+
+      <div className="w-full pb-[75px] px-6 fixed bottom-0 bg-white">
+        <button
+          disabled={name === "" || phone_number === ""}
+          form="form"
+          className={`w-full h-[40px] rounded-lg md:rounded-xl duration-200 text-white md:text-[18px] font-medium ${
+            name === "" || phone_number === ""
+              ? "bg-[#d7d9db]"
+              : "bg-mainColor active:bg-[#23b574]"
+          }`}
+        >
+          Kirish
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -10,22 +10,22 @@ interface CardProps {
 
 const ProductCard = ({ image, title, price }: CardProps) => {
   return (
-    <div className="xl:min-w-[205px] xl:max-w-[205px] h-[300px] md:min-h-[340px] md:max-h-[340px] bg-[#F3F3F8] rounded-xl p-2">
+    <div className="xl:min-w-[205px] xl:max-w-[205px] h-[270px] md:min-h-[330px] md:max-h-[330px] bg-[#F3F3F8] rounded-xl p-2">
       <div className="w-full h-[55%] mb-1 rounded-xl overflow-hidden">
         <Image className="w-full h-full object-cover" src={image} alt="image" />
       </div>
       <div className="w-full h-[45%] flex flex-col justify-between p-1">
         <div>
-          <p className="text-[20px] md:text-[24px] font-semibold">
-            {price.toLocaleString()} so`m
+          <p className="text-[18px] md:text-[24px] font-semibold">
+            {price.toLocaleString("fr-FR")} so`m
           </p>
-          <p className="text-[18px] line-clamp-1 md:line-clamp-2 text-gray-600">{title}</p>
+          <p className="text-[16px] md:text-[18px] line-clamp-2 text-gray-600 leading-5 md:leading-6">{title}</p>
         </div>
-        <button className="flex items-center gap-2 w-full shadow-[0_0_10px_#00000013] bg-white hover:bg-white/50 duration-200 justify-center h-[40px] rounded-xl">
+        <button className="flex items-center gap-2 w-full shadow-[0_0_10px_#00000013] bg-white hover:bg-white/50 duration-200 justify-center h-[35px] md:h-[40px] rounded-xl">
           <span>
-            <FaPlus className="text-[20px]" />
+            <FaPlus className="text-[18px] md:text-[20px]" />
           </span>
-          <span className="text-[18px]">Qo`shish</span>
+          <span className="text-[16px] md:text-[18px]">Qo`shish</span>
         </button>
       </div>
     </div>
